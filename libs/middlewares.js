e64ad5387b871a21cd508f30c352318fffbc8d7a
Souza-Gabriel <gabriel1assunc@gmail.com>
2019-12-21 22:01:49 -0300

    Teste

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,6 +1,12 @@
 const cors = require("cors");
+const bodyParser = require("body-parser");
 module.exports = app => {
   app.set('port', 3001);
   app.set('json spaces', 4);
   app.use(cors());
+  app.use(bodyParser.json());
+  app.use((req, res, next) => {
+    delete req.body.id;
+    next();
+  });
 }
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,42 @@
 module.exports = app => {
   const Usuario = app.db.models.usuario;
   app.get("/user", (req, res) => {
-    Usuario.findAll({}).then((usuario) =>{
-      res.json({Usuario: usuario});
+    Usuario.findAll({})
+      .then(usuario =>{res.json({Usuario: usuario})
+      .catch(error => {
+        error.status(412).json({msg: error.message});
+      });
     });
   });
-  //put
-  //post
-  //delete
+  
+  app.post("/user", (req, res) => {
+    Usuario.create(req.body)
+    .then(result => res.json(result))
+    .catch(error => {
+      res.status(412).json({msg: error.message});
+    });
+  });
+
+  app.get("/user/:id", (req, res) => {
+    Usuario.findOne({where: {id: req.params.id}})
+      .then(usuario => {res.json({Usuario: usuario})})
+      .catch(error => {
+        error.status(412).json({msg: error.message});
+      });
+  });
+
+  app.put("/user/:id", (req, res) => {
+    Usuario.update(req.body, {where: req.params})
+    .then(result => res.sendStatus(204))
+    .catch(error => {
+      error.status(412).json({error: error.message})
+    });
+  });
+
+  app.delete("/user/:id", (req, res) => {
+    Usuario.destroy({where: {id: req.params.id}})
+      .then(result => res.sendStatus(204))
+      .catch(error => res.status(412).json({msg: error.message}));
+  });
+
 }
